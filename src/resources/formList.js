23ae8e2f173ebc9afe51237cf87ff18953e46fab
shuhaib-aot <shuhaib.s@aot-technologies.com>
2022-05-24 12:57:15 +0530

    updated with req.query and env check

--- src/resources/formList.js
+++ src/resources/formList.js
@@ -1,19 +1,20 @@
-
+require("dotenv").config()
 const formList =(req,res,router)=>{
+
     let query={delete:{$eq:null}};
     let titleQuery={}
     const skipForm=req.query.skip||0;
     const limitForm=req.query.limit||0;
     const sortForm = req.query.sort&&req.query.sort==='-title'?-1:1
-    if(req.query){
-        const {type,tags,title__regex}= req.query
-        query={...query,type,tags:tags.split(","),}
-        let regex = title__regex?title__regex.split("/").filter(i=>i):''
-        titleQuery =regex?{title:{$regex:`${regex[0]}`,$options:`${regex[1]}`}}:{}
-    }
+    let {type,tags,title__regex}= req.query
+    type= type?{type}:{}
+    tags=tags?{tags:tags.split(",")}:{}
+    query={...query,...tags,...type}
+    let regex = title__regex?title__regex.split("/").filter(i=>i):''
+    titleQuery =regex?{title:{$regex:`${regex[0]}`,$options:`${regex[1]}`}}:{}
     let {MULTI_TENANCY_ENABLED} = process.env
     let tenantQuery={}
-    if(MULTI_TENANCY_ENABLED&&req.token.tenantId){
+    if(MULTI_TENANCY_ENABLED=='true'&&req.token.tenantId){
         const {tenantId} = req.token
         tenantQuery={tenantId:tenantId}
     }
